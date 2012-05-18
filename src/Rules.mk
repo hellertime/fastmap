fed65a2b92f1d6e6361b6c881089e80b2a22bf0a
Chris Heller <hellertime@gmail.com>
2012-05-17 21:39:38 -0400

    Now building with autotools

--- src/Rules.mk
+++ src/Rules.mk
@@ -1,9 +1,9 @@
 lib_LTLIBRARIES += src/libfastmap.la
 
 src_libfastmap_la_SOURCES = \
-	fastmap_attr.c \
-	fastmap_datumattr.c \
-	fastmap_datum.c \
-	fastmap.c
+	src/fastmap_attr.c \
+	src/fastmap_datumattr.c \
+	src/fastmap_datum.c \
+	src/fastmap.c
 
 src_libfastmap_la_LDFLAGS = -avoid-version
