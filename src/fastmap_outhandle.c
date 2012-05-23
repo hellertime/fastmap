fcc8241ac690f928b6191cfdbb87f849a952489b
Chris Heller <hellertime@gmail.com>
2012-05-22 23:39:44 -0400

    new outhandle code

---
+++ src/fastmap_outhandle.c
@@ -0,0 +1,10 @@
+#ifdef HAVE_CONFIG_H
+#include <fastmap_config.h>
+#endif
+
+#include <fastmap.h>
+
+int fastmap_outhandle_init(fastmap_outhandle_t *ohandle, const fastmap_attr_t *attr, const char *pathname)
+{
+
+}
