e0d4704454bf50ce1598ea22541946c2e1e3e1ba
Chris Heller <hellertime@gmail.com>
2012-06-02 01:05:13 -0400

    removed traces of old API

--- src/Rules.mk
+++ src/Rules.mk
@@ -1,9 +1,6 @@
 lib_LTLIBRARIES += src/libfastmap.la
 
 src_libfastmap_la_SOURCES = \
-	src/fastmap_attr.c \
-	src/fastmap_datumattr.c \
-	src/fastmap_datum.c \
 	src/fastmap.c
 
 src_libfastmap_la_LDFLAGS = -avoid-version
