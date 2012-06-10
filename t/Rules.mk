b5237f69ad56a56d2c66fbf696255cc4c08a15f8
Chris Heller <hellertime@gmail.com>
2012-06-10 00:50:23 -0400

    million non-inline block test

--- t/Rules.mk
+++ t/Rules.mk
@@ -8,6 +8,7 @@ check_PROGRAMS += \
 	t/fastmap_blob_t \
 	t/1M_atom_t \
 	t/1M_pair_t \
+	t/1M_block_t \
 	t/1M_block_inline_t
 
 t_fastmap_attr_t_SOURCES = t/fastmap_attr_t.c
@@ -36,6 +37,9 @@ t_1M_atom_t_LDADD = libtap.a src/libfastmap.la
 
 t_1M_pair_t_SOURCES = t/1M_pair_t.c
 t_1M_pair_t_LDADD = libtap.a src/libfastmap.la
+
+t_1M_block_t_SOURCES = t/1M_block_t.c
+t_1M_block_t_LDADD = libtap.a src/libfastmap.la
 
 t_1M_block_inline_t_SOURCES = t/1M_block_inline_t.c
 t_1M_block_inline_t_LDADD = libtap.a src/libfastmap.la
